0d018573314fcc3a517c93ba039989051bdfb08a
wkim97 <woojae1123@gmail.com>
2020-06-22 21:59:20 +0900

    add predict_image to main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import train
-import test
+# import train
+import predict
 
 if __name__ == '__main__':
-    # train.train_autoencoder(epoch_plus=0)
-    test.test_autoencoder(epoch_plus=400)
+    # train.train_autoencoder(epoch_plus=155)
+    # test.test_autoencoder(epoch_plus=400)
+    dir = '/home/sgvr/wkim97/EE474/ee474/data/test_images/18.png'
+    predict.predict_image(dir)
